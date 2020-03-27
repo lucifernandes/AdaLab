dbbed0a9d160916f4f92a060565918b1c64efa30
Luci Fernandes <luci.fernandes@alu.ufc.br>
2020-03-27 04:20:02 -0300

    Conexão back-end e front-end inicial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './global.css';
-import Logon from './pages/Logon';
+import Routes from './routes';
 
 //JSX (Javascript XML)
 function App() {
   return (
-    <Logon />
+    <Routes />
   );
 }
 
